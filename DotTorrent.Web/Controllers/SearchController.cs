119ddc2c675079d529cc807293a0c1d440cb9878
Aaron Yarborough <me@aaronjy.me>
2019-09-03 21:43:40 +0100

    Add MVC capability

---
+++ DotTorrent.Web/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotTorrent.Web.Controllers
+{
+    [Route("")]
+    [Route("search")]
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Search.cshtml");
+        }
+    }
+}
