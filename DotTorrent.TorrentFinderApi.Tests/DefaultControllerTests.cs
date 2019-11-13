d331a2de24104645f305705ba8d531c3a2343ea7
Aaron Yarborough <personal.ajy@gmail.com>
2019-11-13 16:41:37 +0000

    Add missing tests for DefaultController

---
+++ DotTorrent.TorrentFinderApi.Tests/DefaultControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using DotTorrent.TorrentFinderApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace DotTorrent.TorrentFinderApi.Tests
+{
+  public class DefaultControllerTests
+  {
+    [Test]
+    public void Index_ShowsGreeting()
+    {
+      var controller = new DefaultController();
+      var result = (ContentResult)controller.Index();
+
+      Assert.AreEqual("We're up!", result.Content);
+    }
+  }
+}
